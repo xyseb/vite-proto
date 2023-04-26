37ec60adfaaac6c18156b7d8e06394344e372c5c
Sebastien Deschamps <sdx007.com@gmail.com>
2023-04-26 07:41:40 +0200

    [vite-plugin-checker] begin impl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,js,mjs,cjs,jsx}"',
+      },
+      stylelint: {
+        lintCommand: 'stylelint ./src/**/*.{scss}',
+      },
+    })
+  ],
 })
